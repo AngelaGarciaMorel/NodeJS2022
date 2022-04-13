9021ccbb163075f047cfd12cead4333d6e50f33f
Angela Garcia <a.garcia.morel@gmail.com>
2022-04-13 02:11:33 -0300

    Motores de plantillas

---
+++ MotoresPlantillas/handlebars/main.js
@@ -0,0 +1,67 @@
+const express = require('express')
+const { Router } = express;
+const handleBars = require('express-handlebars');
+
+const app = express();
+
+const PORT = 8080;
+
+app.engine(
+    'hbs',
+    handleBars({
+        extname:'.hbs',
+        defaultLayout: 'index.hbs',//plantilla principal
+        layoutsDir: __dirname + '/views/layouts',
+        partialsDir: __dirname + '/views/partials'//un parcial puede ser selecciones de la page que siempre se utilizan, header, footer
+    }));
+
+app.set('view engine', 'hbs');
+app.set('views','./views');
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+
+
+//lista de productos
+const listaProductos = [];
+
+//Servicios
+app.get('/', (req, res) => {
+    res.render('cargaProductos');
+});
+
+app.get('/productos', (req, res) => {
+    let tieneDatos = false;
+    if (listaProductos.length > 0) {
+        tieneDatos = true;
+    }
+    res.render('listaProductos', {productos: listaProductos, tieneDatos});
+});
+
+
+
+app.post('/productos', (req, res) => {
+    let product = req.body;
+    let newId = 0;
+
+    for (let index = 0; index < listaProductos.length; index++) {
+        if(newId < listaProductos[index].id){
+            newId = listaProductos[index].id;
+        }
+    }
+    newId = newId +1;
+    product.id = newId;
+
+    listaProductos.push(product);
+    console.log(listaProductos);
+    res.render('cargaProductos');
+    
+});
+
+const server = app.listen(PORT, () => {
+    console.log(`Servidor escuchando en el puerto ${server.address().port}`)
+})
+server.on('error', error => console.log(`Error en servidor ${error}`))
