86f00e44f5b17baa3c9bae01ff8ed98d54e79d2d
Angela Garcia <a.garcia.morel@gmail.com>
2022-05-19 00:03:47 -0300

    primera subida

--- SegundaEntrega/src/config.js
+++ SegundaEntrega/src/config.js
@@ -3,17 +3,27 @@ export default {
         
     },
     mongodb: {
-        
+        URL: 'mongodb://localhost:27017/ecommerce'       
     },
     firebase: {
-        
+        serviceAccount: require("./db/ecommerce-7c1fd-firebase-adminsdk-d986t-7d7bdf86c1.json"),
+        credential: admin.credential.cert(serviceAccount),
+        databaseURL: 'https://ecommerce-7c1fd.firebaseio.com'       
     },
     sqlite3: {
-       connection: {
-           filename: './'
-       }
+        client: 'sqlite3',
+        connection: {
+            filename: "./DB/ecommerce.sqlite"
+        },
+        useNullAsDefault: true
     },
     mariaDb: {
-        
+        client: 'mysql',
+        connection: {
+            host: '127.0.0.1',
+            user: 'root',
+            password: '',
+            database:'ecommerce'
+        }        
     }
 }
